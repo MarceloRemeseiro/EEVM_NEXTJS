c40330425c58eef1b464abdb185520fcfa58d9ec
MarceloRemeseiro <marcelo.remeseiro@gmail.com>
2022-12-24 12:27:50 +0100

    revision

--- components/testimonios/testimonios.js
+++ components/testimonios/testimonios.js
@@ -24,22 +24,6 @@ function Testimonios() {
               diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet
               lorem sit clita duo justo magna dolore erat amet
             </p>
-            {/* <div className="row g-4">
-              <div className="col-sm-6">
-                <div className="d-flex align-items-center mb-2">
-                  <i className="fa fa-users fa-2x text-primary flex-shrink-0"></i>
-                  <h1 className="ms-3 mb-0">123+</h1>
-                </div>
-                <h5 className="mb-0">Happy Clients</h5>
-              </div>
-              <div className="col-sm-6">
-                <div className="d-flex align-items-center mb-2">
-                  <i className="fa fa-check fa-2x text-primary flex-shrink-0"></i>
-                  <h1 className="ms-3 mb-0">123+</h1>
-                </div>
-                <h5 className="mb-0">Projects Done</h5>
-              </div>
-            </div> */}
           </div>
           <div
             className="col-lg-7 wow fadeInUp overflow-scroll"
@@ -89,45 +73,6 @@ function Testimonios() {
               </div>
               <hr />
             </div>
-
-            {/* <div className="carousel testimonial-carousel">
-              <div className="testimonial-item">
-                <img
-                  className="img-fluid mb-4"
-                  src="img/testimonial-1.jpg"
-                  alt=""
-                />
-                <p className="fs-5">
-                  Dolores sed duo clita tempor justo dolor et stet lorem kasd
-                  labore dolore lorem ipsum. At lorem lorem magna ut et, nonumy
-                  et labore et tempor diam tempor erat.
-                </p>
-                <div
-                  className="bg-primary mb-3"
-                  style={{width: "60px", height: "5px"}}
-                ></div>
-                <h5>Nombre</h5>
-                <span>Profesión</span>
-              </div>
-              <div className="testimonial-item">
-                <img
-                  className="img-fluid mb-4"
-                  src="img/testimonial-2.jpg"
-                  alt=""
-                />
-                <p className="fs-5">
-                  Dolores sed duo clita tempor justo dolor et stet lorem kasd
-                  labore dolore lorem ipsum. At lorem lorem magna ut et, nonumy
-                  et labore et tempor diam tempor erat.
-                </p>
-                <div
-                  className="bg-primary mb-3"
-                  style={{width: "60px", height: "5px"}}
-                ></div>
-                <h5>Nombre</h5>
-                <span>Profesión</span>
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
